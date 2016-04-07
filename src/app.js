3d1db53bc939e794613a7af396b91d1022be449f
yellowChao <yellowchao91@163.com>
2016-04-07 21:12:18 +0800

    project create and support es6

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    console.log(req);
+    res.send('welcome to hollywood!')
+})
+
+const server = app.listen(8000,() => {
+    const port = server.address().port
+    console.log(`hollywood server start on http://127.0.0.1:${port}`)
+})
---
+++ src/index.js
@@ -0,0 +1,3 @@
+// entrance file, dont change
+require('babel-core/register')
+require('./app')
